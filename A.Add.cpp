efe96ead059ac29d6ae278de7c4be6520e8f51a6
Eyad Almallah <eyad.almallah09@outlook.com>
2023-03-04 20:46:07 +0200

    Sheet 5 || PS

---
+++ A.Add.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int a,int b);
+int main()
+{
+ int x , y ;
+ cin >> x >> y;
+ sum(x,y);
+ cout << x + y;
+}
+
+int sum (int A, int B){
+int res = A + B;
+return res;
+}
+
